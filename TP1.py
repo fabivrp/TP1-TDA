bc98ee181b7def6127f149c9286cf52333b347f8
fabivrp <fromerop@fi.uba.ar>
2024-09-18 23:12:32 -0300

    Código actualizado +  cálculo complejidad y gráficos

--- TP1.py
+++ TP1.py
@@ -1,13 +1,3 @@
-class EnteroMutable:
-    def __init__(self, valor):
-        self.valor = valor
-
-    def sumar(self, cantidad=1):
-        self.valor += cantidad
-    
-    def devolver_valor(self):
-        return self.valor 
-
 import sys
 
 class EnteroMutable:
@@ -19,7 +9,6 @@ class EnteroMutable:
     
     def devolver_valor(self):
         return self.valor 
-
 
 def leer_monedas_de_archivo(archivo):
     
@@ -46,12 +35,8 @@ def leer_monedas_de_archivo(archivo):
         print("Error: Asegúrate de que todos los valores en el archivo son números enteros válidos.")
         return []
 
-
-
-
 def moneda_inicial_es_mayor(monedas, inicio, fin):
    return (monedas[inicio] >= monedas[fin])
-
 
 def seleccionar_moneda(monedas, inicio, fin, jugador, puntos_sofia, puntos_mateo):
    
@@ -70,8 +55,6 @@ def seleccionar_moneda(monedas, inicio, fin, jugador, puntos_sofia, puntos_mateo
             puntos_mateo.sumar(monedas[inicio.devolver_valor()])
             inicio.sumar()
        
-  
-
 def jugar(monedas):
     inicio = EnteroMutable(0)
     fin = EnteroMutable(len(monedas) - 1)
@@ -79,13 +62,12 @@ def jugar(monedas):
     puntos_mateo = EnteroMutable(0)
 
 
-    for i in range (1, len(monedas) + 1):
-        seleccionar_moneda(monedas, inicio, fin, i, puntos_sofia, puntos_mateo)
+    for i in range  (len(monedas)):
+        seleccionar_moneda(monedas, inicio, fin, i+1, puntos_sofia, puntos_mateo)
   
   
     print(f"Puntos sofi: {puntos_sofia.devolver_valor()}")
     print(f"Puntos mateo: {puntos_mateo.devolver_valor()}")
-
 
 if __name__ == "__main__":
   
@@ -94,51 +76,7 @@ if __name__ == "__main__":
         sys.exit(1)
     
     archivo = sys.argv[1]
-     
+
     monedas = leer_monedas_de_archivo(archivo)
     
-    if monedas:
-        jugar(monedas)
-
-def moneda_inicial_es_mayor(monedas, inicio, fin):
-   return (monedas[inicio] >= monedas[fin])
-
-
-def seleccionar_moneda(monedas, inicio, fin, jugador, puntos_sofia, puntos_mateo):
-   
-    if jugador % 2 != 0:
-        if moneda_inicial_es_mayor(monedas, inicio.devolver_valor(), fin.devolver_valor()):
-            puntos_sofia.sumar(monedas[inicio.devolver_valor()])
-            inicio.sumar()
-        else: 
-            puntos_sofia.sumar(monedas[fin.devolver_valor()])
-            fin.sumar(-1)
-    else:
-        if moneda_inicial_es_mayor(monedas, inicio.devolver_valor(), fin.devolver_valor()):
-            puntos_mateo.sumar(monedas[fin.devolver_valor()])
-            fin.sumar(-1)
-        else: 
-            puntos_mateo.sumar(monedas[inicio.devolver_valor()])
-            inicio.sumar()
-       
-  
-
-def jugar(monedas):
-    inicio = EnteroMutable(0)
-    fin = EnteroMutable(len(monedas) - 1)
-    puntos_sofia = EnteroMutable(0)
-    puntos_mateo = EnteroMutable(0)
-
-
-    for i in range (1, len(monedas) + 1):
-        seleccionar_moneda(monedas, inicio, fin, i, puntos_sofia, puntos_mateo)
-  
-  
-    print(f"Puntos sofi: {puntos_sofia.devolver_valor()}")
-    print(f"Puntos mateo: {puntos_mateo.devolver_valor()}")
-
-
-if __name__ == "__main__":
-
-    monedas =[406,691,451,628,950,324,906,34,345,647,589,585,728,338,598,362,999,227,248,863,852,344,166,153,778]
-    jugar(monedas)
+    if monedas: jugar(monedas)
